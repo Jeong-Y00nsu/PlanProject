e1155ee57f1739de80c38a7e48bfc4b62befecd6
main2039@gmail.com <main2039@gmail.com>
2024-04-02 06:47:07 +0900

    DB 연동 test

---
+++ src/main/java/com/jRyun/demo/planProject/config/PlanMapperConfig.java
@@ -0,0 +1,50 @@
+package com.jRyun.demo.planProject.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(value = "com.jRyun.demo.planProject.plan.mapper", sqlSessionFactoryRef = "planFactory")
+public class PlanMapperConfig {
+
+    @Primary
+    @Bean(name="planDatasource")
+    @ConfigurationProperties(prefix = "spring.datasource.plan")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean(name="planSqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        sessionFactory.setTypeAliasesPackage("com.jRyun.demo.planProject.plan");
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sessionFactory.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
+
+        Resource myBatisConfig = new PathMatchingResourcePatternResolver().getResource("classpath:mybatis-config.xml");
+        sessionFactory.setConfigLocation(myBatisConfig);
+
+        return sessionFactory.getObject();
+    }
+
+    @Primary
+    @Bean(name="planSqlSession")
+    public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory){
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+}
---
+++ src/main/java/com/jRyun/demo/planProject/config/UserMapperConfig.java
@@ -0,0 +1,39 @@
+package com.jRyun.demo.planProject.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(value = "com.jRyun.demo.planProject.user.mapper", sqlSessionFactoryRef = "userFactory")
+public class UserMapperConfig {
+
+    @Bean(name="userDatasource")
+    @ConfigurationProperties(prefix = "spring.datasource.user")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name="userSqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactory(@Qualifier("userDatasource")DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource);
+        sqlSessionFactory.setTypeAliasesPackage("com.jRyun.demo.planProject.user");
+        sqlSessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+        return sqlSessionFactory.getObject();
+    }
+
+    @Bean(name="userSqlSession")
+    public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory){
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
